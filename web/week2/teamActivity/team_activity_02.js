18a61fa77b19027abc713865b50a815868bbd452
Will Von Doersten <von16004@byui.edu>
2020-01-14 16:52:30 -0700

    added team activity

---
+++ web/week2/teamActivity/team_activity_02.js
@@ -0,0 +1,7 @@
+function userClick() {
+    alert("Clicked!");
+}
+function changeColor(){
+    document.getElementById('first').style.backgroundColor = document.getElementById('newColor').value;
+}
+
