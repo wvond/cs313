8df5e92ae1eccc88240d7b5efef8c4de35c26e09
wvond <von16004@byui.edu>
2020-01-21 15:50:26 -0700

    updated index file

--- web/index.php
+++ web/index.php
@@ -1,37 +1,33 @@
-<?php
+<!DOCTYPE html>
+<html lang='en'>
 
-  echo "<!DOCTYPE html>
-  <html lang='en'>
-  
-  <head>
-      <meta charset='utf-8' />
-      <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-      <title>Home Page</title>
-      <link rel='stylesheet' type='text/css' href='shared/header-nav.css'>
-      <link rel='stylesheet' type='text/css' href='shared/boot-css/bootstrap-grid.css'>
-  </head>
-  
-  <body>";
+<head>
+  <meta charset='utf-8' />
+  <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+  <title>Home Page</title>
+  <link rel='stylesheet' type='text/css' href='shared/header-nav.css'>
+  <link rel='stylesheet' type='text/css' href='shared/boot-css/bootstrap-grid.css'>
+</head>
 
-  include 'shared/header.php';
+<body>
 
-  echo '<div class="main-content">
-  <h2>About The Author:</h2>
+  <?php include 'shared/header.php'; ?>
+
+  <div class="main-content">
+    <h2>About The Author:</h2>
   </div>
   <div class="col">
-  <h3>Favorite Quote:</h3>
-  <img src="shared/images/mcgregor.jpg">
+    <h3>Favorite Quote:</h3>
+    <img src="shared/images/mcgregor.jpg">
   </div>
 
   <div class="col">
-  <h3>From Austin, Texas</h3>
-  <img src="shared/images/atx.jpg">
-  </div>';
+    <h3>From Austin, Texas</h3>
+    <img src="shared/images/atx.jpg">
+  </div>
 
-  include 'shared/footer.php';
-  
-  echo '</body>
+  <?php include 'shared/footer.php'; ?>
 
-  </html>';
+</body>
 
-?>
+</html>
