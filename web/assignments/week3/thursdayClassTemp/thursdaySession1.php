97c87c943ed56723df27fe13a5284ef150ab9be7
wvond <von16004@byui.edu>
2020-01-23 16:31:09 -0700

    add session variable

--- web/assignments/week3/thursdayClassTemp/thursdaySession1.php
+++ web/assignments/week3/thursdayClassTemp/thursdaySession1.php
@@ -1,31 +1,33 @@
 <?php
-   // Start the session
-   session_start();
+// Start the session
+session_start();
 ?>
 <!DOCTYPE html>
 <html>
-   <body>
-      <?php
-         // remove previous session variable
-         //unset($_SESSION['pictureUrl']);
-         // Set session variables
-         $_SESSION["favcolor"] = "green";
-         $_SESSION["favanimal"] = "dolphin";
-         // echo that variables have been set
-         echo "Session variables have been set.";
-?>
-      <a href="thursdaySession2.php">Check the variables on another page</a>
 
-      <h3>Just for kicks, let's try this in a form!</h3>
-      <form action="" method="post">
-         <input type="text" name="picture">
-         <input type="submit" name="Submit" value="Submit!">
-      </form>
-      <?php // set session variables using a form 
-         if(isset($_POST['Submit'])) {
-            $_SESSION['pictureUrl'] = $_POST['picture'];
-         }
+<body>
+   <?php
+   // remove previous session variable
+   //unset($_SESSION['pictureUrl']);
+   // Set session variables
+   $_SESSION["favcolor"] = "green";
+   $_SESSION["favanimal"] = "dolphin";
+   // echo that variables have been set
+   echo "Session variables have been set.";
+   ?>
+   <a href="thursdaySession2.php">Check the variables on another page</a>
 
-      ?>
-   </body>
+   <h3>Just for kicks, let's try this in a form!</h3>
+   <form action="" method="post">
+      <input type="text" name="picture">
+      <input type="submit" name="Submit" value="Submit!">
+   </form>
+   <?php // set session variables using a form 
+   if (isset($_POST['Submit'])) {
+      $_SESSION['pictureUrl'] = $_POST['picture'];
+   }
+
+   ?>
+</body>
+
 </html>
--- web/assignments/week3/thursdayClassTemp/thursdaySession2.php
+++ web/assignments/week3/thursdayClassTemp/thursdaySession2.php
@@ -1,14 +1,14 @@
-<?php 
-   // start session
-   session_start();
-   // save session variables into local variables
-   $c = $_SESSION['favcolor'];
-   $a = $_SESSION['favanimal'];
+<?php
+// start session
+session_start();
+// save session variables into local variables
+$c = $_SESSION['favcolor'];
+$a = $_SESSION['favanimal'];
 ?>
-<h1>Your favorite color is <?=$c?> and your favorite animal is <?=$a?></h1>
+<h1>Your favorite color is <?= $c ?> and your favorite animal is <?= $a ?></h1>
 
-<?php if(isset($_SESSION['pictureUrl'])) {?>
+<?php if (isset($_SESSION['pictureUrl'])) { ?>
    <h3>Again, just for kicks... from a form</h3>
-   <img src="<?=$_SESSION['pictureUrl'] ?>" />
+   <img src="<?= $_SESSION['pictureUrl'] ?>" />
 
 <?php } ?>
