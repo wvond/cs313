17300e730e7754ddd08b9e655b3dc68468618aa1
wvond <von16004@byui.edu>
2020-01-23 16:16:11 -0700

    add session variable using form session2

--- web/assignments/week3/thursdayClassTemp/thursdaySession2.php
+++ web/assignments/week3/thursdayClassTemp/thursdaySession2.php
@@ -5,5 +5,10 @@
    $c = $_SESSION['favcolor'];
    $a = $_SESSION['favanimal'];
 ?>
-<h1>Your favorite color is <?$c?> and your favorite animal is <?$a?></h1>
-<?php ?>
+<h1>Your favorite color is <?=$c?> and your favorite animal is <?=$a?></h1>
+
+<?php if(isset($_SESSION['pictureUrl'])) {?>
+   <h3>Again, just for kicks... from a form</h3>
+   <img src="<?=$_SESSION['pictureUrl'] ?>" />
+
+<?php } ?>
