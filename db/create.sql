b55c6ee6f5e6d3fef6b3fdd46f3bd0ea9b45a56b
wvond <von16004@byui.edu>
2020-02-06 15:00:14 -0700

    run db commands and update db files

--- db/create.sql
+++ db/create.sql
@@ -1,28 +1,28 @@
-DROP TABLE IF EXISTS photo;
+/* DROP TABLE IF EXISTS photo;
 DROP TABLE IF EXISTS catch;
 DROP TABLE IF EXISTS species;
 DROP TABLE IF EXISTS location;
-DROP TABLE IF EXISTS fish;
+DROP TABLE IF EXISTS fish; */
 
-CREATE TABLE photo
+CREATE TABLE IF NOT EXISTS photo
 ( photo_id      SERIAL  NOT NULL    PRIMARY KEY
 , photo_url     TEXT
 );
 
-CREATE TABLE catch
+CREATE TABLE IF NOT EXISTS catch
 ( catch_id      SERIAL  NOT NULL    PRIMARY KEY
 , catch_method  VARCHAR(50)
 , catch_notes   TEXT
 );
 
-CREATE TABLE species
+CREATE TABLE IF NOT EXISTS species
 ( species_id          SERIAL  NOT NULL    PRIMARY KEY
 , species_scientific  VARCHAR(50)
 , species_common      VARCHAR(50)
 , species_details     TEXT
 );
 
-CREATE TABLE location
+CREATE TABLE IF NOT EXISTS location
 ( location_id           SERIAL  NOT NULL    PRIMARY KEY
 , city_state            VARCHAR(50)
 , location_details      TEXT
@@ -30,14 +30,14 @@ CREATE TABLE location
 , access_type           VARCHAR(50)
 );
 
-CREATE TABLE fish
+CREATE TABLE IF NOT EXISTS fish
 ( fish_id     SERIAL      NOT NULL    PRIMARY KEY
 , fish_weight NUMERIC   
 , length      NUMERIC
 , time_caught TIME
 , date_caught DATE 
-, location_id INT       FOREIGN KEY
-, species_id  INT       FOREIGN KEY
-, photo_id    INT       FOREIGN KEY
-, catch_id    INT       FOREIGN KEY  
+, location_id INT       NOT NULL    REFERENCES location(location_id)
+, species_id  INT       NOT NULL    REFERENCES species(species_id)
+, photo_id    INT       NOT NULL    REFERENCES photo(photo_id)
+, catch_id    INT       NOT NULL    REFERENCES catch(catch_id)  
 );
