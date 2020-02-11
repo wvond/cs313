c5a3b31762c210a1b4f68649589f30e2f0973665
wvond <von16004@byui.edu>
2020-02-11 15:47:32 -0700

    update week6 tuesday class activity

--- web/assignments/week6/tuesday/display.php
+++ web/assignments/week6/tuesday/display.php
@@ -1,16 +1,36 @@
 <?php
-	require("dbConnect.php");
-	$db = get_db();
+require("dbConnect.php");
+$db = get_db();
 ?>
-	<body>
-		<div class="container">
-         <?php
-            // retrieve url parameter
-            // execute query to pull up data from that id
-            // execute another query to get food data
-            // display name and favorite food
-         ?>
 
-		</div>
-	</body>
+<body>
+   <div class="container">
+      <?php
+      $personId = $_GET['personId'];
+      $statement = $db->prepare('SELECT * FROM w6_user WHERE ID = :personId');
+      $statement->bindValue(':personId', $personId);
+      $statement->execute();
+      while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+         $id = $row['id'];
+         $first = $row['first'];
+         $last = $row['last'];
+         $food_id = $row['food_type'];
+
+         $foods = $db->prepare('SELECT food FROM w6_food WHERE ID = $food_id');
+         $foods->execute();
+         while ($fRow = $foods->fetch(PDO::FETCH_ASSOC)) {
+            $food = $fRow['food'];
+         }
+         echo "<h1>$fist $last's favorite food is $food</h1>";
+      }
+
+
+      // execute query to pull up data from that id
+      // execute another query to get food data
+      // display name and favorite food
+      ?>
+
+   </div>
+</body>
+
 </html>
--- web/assignments/week6/tuesday/insert.php
+++ web/assignments/week6/tuesday/insert.php
@@ -1,15 +1,26 @@
 <?php
 // retrieve POST data from the other page
+$first = $_POST['first'];
+$last = $_POST['last'];
+$food = $_POST['food'];
+
 
 require("dbConnect.php");
 $db = get_db();
 
 try
 {
-	// insert into database
+	$query = 'INSERT INTO w6_user (first_name, last_name, food_type) VALUES (:first, :last, :food)';
+	$statement = $db->prepare($query);
+	$statement->bindValue(':first', $first);
+	$statement->bindValue(':last', $last);
+	$statement->bindValue(':food', $food);
+	$statement->execute();
+
 	
 	// SELECT c.relname FROM pg_class c WHERE c.relkind = 'S';   -- display all sequences
 	// get id of last inserted row - save in $userId
+	$userId = $db->lastInsertId("w6_user_id_seq");
 }
 catch (Exception $ex)
 {
