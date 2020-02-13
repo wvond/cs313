482bfc433e47568a2c1f395bc28c87456d42750f
wvond <von16004@byui.edu>
2020-02-13 15:50:03 -0700

    update db_search file

--- web/assignments/week5/db_search.php
+++ web/assignments/week5/db_search.php
@@ -13,23 +13,24 @@ session_start();
 <body>
     <h1>See All Fish Records</h1>
     <div>
-    <?php
-    require "dbConnect.php";
-    $db = get_db();
+        <ol>
+            <?php
+            require "dbConnect.php";
+            $db = get_db();
 
-    $fish = $db->prepare("SELECT * FROM fish;");
-    $fish->execute();
+            $fish = $db->prepare("SELECT * FROM fish;");
+            $fish->execute();
 
-    while ($row = $fish->fetch(PDO::FETCH_ASSOC)) {
-        $id = $row['fish_id'];
-        $weight = $row['fish_weight'];
-        $time = $row['time_caught'];
-        $date = $row['date_caught'];
-    }
+            while ($row = $fish->fetch(PDO::FETCH_ASSOC)) {
+                $id = $row['fish_id'];
+                $weight = $row['fish_weight'];
+                $time = $row['time_caught'];
+                $date = $row['date_caught'];
 
-    echo "<p>Fish #$id weighs $weight pounds and was caught on $date at $time.</p>"
-
-    ?>
+                echo "<li>Fish #$id weighs $weight pounds and was caught on $date at $time.</li>";
+            }
+            ?>
+        </ol>
     </div>
     <!-- [SEARCH RESULTS] -->
     <div id="results"></div>
